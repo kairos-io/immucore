1e4df671bfb6097fc6bbbb1a9609c61405666026
Itxaka <itxaka.garcia@spectrocloud.com>
2023-02-15 22:27:05 +0100

    Adapt tests
    Signed-off-by: Itxaka <itxaka.garcia@spectrocloud.com>

--- go.mod
+++ go.mod
@@ -30,9 +30,11 @@ require (
 	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20210107165309-348f09dbbbc0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38 // indirect
 	github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 // indirect
 	github.com/gookit/color v1.5.2 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
@@ -61,6 +63,7 @@ require (
 	golang.org/x/sys v0.4.0 // indirect
 	golang.org/x/term v0.4.0 // indirect
 	golang.org/x/text v0.6.0 // indirect
+	golang.org/x/tools v0.5.0 // indirect
 	gopkg.in/yaml.v1 v1.0.0-20140924161607-9f9df34309c0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
--- pkg/mount/state_test.go
+++ pkg/mount/state_test.go
@@ -17,12 +17,12 @@ var _ = Describe("mounting immutable setup", func() {
 	var g *herd.Graph
 
 	BeforeEach(func() {
-		g = herd.DAG()
+		g = herd.DAG(herd.EnableInit)
 		Expect(g).ToNot(BeNil())
 	})
 
 	Context("simple invocation", func() {
-		It("mounts base overlay, attempt to mount oem, and updates the fstab", func() {
+		It("generates normal dag", func() {
 			s := &mount.State{
 				Logger:      log.Output(zerolog.ConsoleWriter{Out: os.Stderr}).With().Logger(),
 				Rootdir:     "/",
@@ -36,96 +36,102 @@ var _ = Describe("mounting immutable setup", func() {
 
 			dag := g.Analyze()
 
-			Expect(len(dag)).To(Equal(9), s.WriteDAG(g))
-			Expect(len(dag[0])).To(Equal(2), s.WriteDAG(g))
-			Expect(len(dag[1])).To(Equal(1), s.WriteDAG(g))
-			Expect(len(dag[2])).To(Equal(1), s.WriteDAG(g))
-			Expect(len(dag[3])).To(Equal(1), s.WriteDAG(g))
-			Expect(len(dag[4])).To(Equal(1), s.WriteDAG(g))
-			Expect(len(dag[5])).To(Equal(1), s.WriteDAG(g))
-			Expect(len(dag[6])).To(Equal(2), s.WriteDAG(g))
-			Expect(len(dag[7])).To(Equal(1), s.WriteDAG(g))
-			Expect(len(dag[8])).To(Equal(1), s.WriteDAG(g))
-
-			Expect(dag[0][0].Name).To(Or(Equal("mount-base-overlay"), Equal("mount-state")), s.WriteDAG(g))
-			Expect(dag[0][1].Name).To(Or(Equal("mount-base-overlay"), Equal("mount-state")), s.WriteDAG(g))
-			Expect(dag[1][0].Name).To(Equal("discover-state"), s.WriteDAG(g))
-			Expect(dag[2][0].Name).To(Equal("mount-root"), s.WriteDAG(g))
-			Expect(dag[3][0].Name).To(Equal("mount-oem"), s.WriteDAG(g))
-			Expect(dag[4][0].Name).To(Equal("rootfs-hook"), s.WriteDAG(g))
-			Expect(dag[5][0].Name).To(Equal("load-config"), s.WriteDAG(g))
-			Expect(dag[6][0].Name).To(Or(Equal("overlay-mount"), Equal("custom-mount")), s.WriteDAG(g))
-			Expect(dag[6][1].Name).To(Or(Equal("overlay-mount"), Equal("custom-mount")), s.WriteDAG(g))
-			Expect(dag[7][0].Name).To(Equal("mount-bind"), s.WriteDAG(g))
-			Expect(dag[8][0].Name).To(Equal("write-fstab"), s.WriteDAG(g))
-		})
-
-		It("mounts base overlay, attempt to mount oem, and updates the fstab", func() {
-			s := &mount.State{Rootdir: "/", MountRoot: true}
-
-			s.RegisterNormalBoot(g)
-
-			dag := g.Analyze()
-
-			Expect(len(dag)).To(Equal(9), s.WriteDAG(g))    // Expect 4 layers
-			Expect(len(dag[0])).To(Equal(2), s.WriteDAG(g)) // 2 items in first layer
-			Expect(len(dag[1])).To(Equal(1))                // 1 Item for each layer, as are tight deps
-			Expect(len(dag[2])).To(Equal(1))
-			Expect(len(dag[3])).To(Equal(1))
-			Expect(len(dag[6])).To(Equal(2))
-
-			Expect(dag[0][0].Name).To(Or(Equal("mount-base-overlay"), Equal("mount-state")), s.WriteDAG(g))
-			Expect(dag[0][1].Name).To(Or(Equal("mount-base-overlay"), Equal("mount-state")), s.WriteDAG(g))
-			Expect(dag[1][0].Name).To(Equal("discover-state"))
-			Expect(dag[2][0].Name).To(Equal("mount-root"))
-			Expect(dag[3][0].Name).To(Equal("mount-oem"))
-			Expect(dag[4][0].Name).To(Equal("rootfs-hook"))
-			Expect(dag[5][0].Name).To(Equal("load-config"))
-			Expect(dag[6][0].Name).To(Or(Equal("overlay-mount"), Equal("custom-mount")), s.WriteDAG(g))
-			Expect(dag[6][1].Name).To(Or(Equal("overlay-mount"), Equal("custom-mount")), s.WriteDAG(g))
-
-			Expect(dag[7][0].Name).To(Equal("mount-bind"))
-			Expect(dag[8][0].Name).To(Equal("write-fstab"))
+			checkDag(dag, s.WriteDAG(g))
 
 		})
-
-		It("mounts all", func() {
+		It("generates normal dag with extra dirs", func() {
 			s := &mount.State{Rootdir: "/", MountRoot: true,
 				OverlayDirs:  []string{"/etc"},
 				BindMounts:   []string{"/etc/kubernetes"},
 				CustomMounts: map[string]string{"COS_PERSISTENT": "/usr/local"}}
 
-			s.RegisterNormalBoot(g)
+			err := s.RegisterNormalBoot(g)
+			Expect(err).ToNot(HaveOccurred())
 
 			dag := g.Analyze()
 
-			Expect(len(dag)).To(Equal(9), s.WriteDAG(g))    // Expect 6 layers
-			Expect(len(dag[0])).To(Equal(2), s.WriteDAG(g)) // 2 items in first layer
-			Expect(len(dag[1])).To(Equal(1))                // 1 Item for each layer, as are tight deps
-			Expect(len(dag[2])).To(Equal(1))
-			Expect(len(dag[3])).To(Equal(1))
-			Expect(len(dag[6])).To(Equal(2))
+			checkDag(dag, s.WriteDAG(g))
+		})
+		It("generates livecd dag", func() {
+			s := &mount.State{}
+			err := s.RegisterLiveMedia(g)
+			Expect(err).ToNot(HaveOccurred())
+			dag := g.Analyze()
+			checkLiveCDDag(dag, s.WriteDAG(g))
 
-			Expect(dag[0][0].Name).To(Or(Equal("mount-base-overlay"), Equal("mount-state")), s.WriteDAG(g))
-			Expect(dag[0][1].Name).To(Or(Equal("mount-base-overlay"), Equal("mount-state")), s.WriteDAG(g))
-			Expect(dag[1][0].Name).To(Equal("discover-state"))
-			Expect(dag[2][0].Name).To(Equal("mount-root"))
-			Expect(dag[3][0].Name).To(Equal("mount-oem"))
-			Expect(dag[4][0].Name).To(Equal("rootfs-hook"))
-			Expect(dag[5][0].Name).To(Equal("load-config"))
-			Expect(dag[6][0].Name).To(Or(Equal("overlay-mount"), Equal("custom-mount")), s.WriteDAG(g))
-			Expect(dag[6][1].Name).To(Or(Equal("overlay-mount"), Equal("custom-mount")), s.WriteDAG(g))
-
-			Expect(dag[7][0].Name).To(Equal("mount-bind"))
-			Expect(dag[8][0].Name).To(Equal("write-fstab"))
 		})
 
 		It("Mountop timeouts", func() {
 			s := &mount.State{}
-			f := s.MountOP("/dev/doesntexist", "/tmp", "", []string{}, 1*time.Second)
+			f := s.MountOP("/dev/doesntexist", "/tmp/jojobizarreadventure", "", []string{}, 500*time.Millisecond)
 			err := f(context.Background())
 			Expect(err).To(HaveOccurred())
 			Expect(err.Error()).To(ContainSubstring("exhausted"))
 		})
 	})
 })
+
+func checkLiveCDDag(dag [][]herd.GraphEntry, actualDag string) {
+	Expect(len(dag)).To(Equal(2), actualDag)
+	Expect(len(dag[0])).To(Equal(1), actualDag)
+	Expect(len(dag[1])).To(Equal(2), actualDag)
+
+	Expect(dag[0][0].Name).To(Equal("init"))
+	Expect(dag[1][0].Name).To(Or(
+		Equal("mount-tmpfs"),
+		Equal("create-sentinel"),
+	), actualDag)
+	Expect(dag[1][1].Name).To(Or(
+		Equal("mount-tmpfs"),
+		Equal("create-sentinel"),
+	), actualDag)
+
+}
+func checkDag(dag [][]herd.GraphEntry, actualDag string) {
+	Expect(len(dag)).To(Equal(10), actualDag)
+	Expect(len(dag[0])).To(Equal(1), actualDag)
+	Expect(len(dag[1])).To(Equal(4), actualDag)
+	Expect(len(dag[2])).To(Equal(1), actualDag)
+	Expect(len(dag[3])).To(Equal(1), actualDag)
+	Expect(len(dag[4])).To(Equal(1), actualDag)
+	Expect(len(dag[5])).To(Equal(1), actualDag)
+	Expect(len(dag[6])).To(Equal(1), actualDag)
+	Expect(len(dag[7])).To(Equal(2), actualDag)
+	Expect(len(dag[8])).To(Equal(1), actualDag)
+	Expect(len(dag[9])).To(Equal(1), actualDag)
+
+	Expect(dag[0][0].Name).To(Equal("init"))
+	Expect(dag[1][0].Name).To(Or(
+		Equal("mount-tmpfs"),
+		Equal("create-sentinel"),
+		Equal("mount-base-overlay"),
+		Equal("mount-state"),
+	), actualDag)
+	Expect(dag[1][1].Name).To(Or(
+		Equal("mount-tmpfs"),
+		Equal("create-sentinel"),
+		Equal("mount-base-overlay"),
+		Equal("mount-state"),
+	), actualDag)
+	Expect(dag[1][2].Name).To(Or(
+		Equal("mount-tmpfs"),
+		Equal("create-sentinel"),
+		Equal("mount-base-overlay"),
+		Equal("mount-state"),
+	), actualDag)
+	Expect(dag[1][3].Name).To(Or(
+		Equal("mount-tmpfs"),
+		Equal("create-sentinel"),
+		Equal("mount-base-overlay"),
+		Equal("mount-state"),
+	), actualDag)
+	Expect(dag[2][0].Name).To(Equal("discover-state"), actualDag)
+	Expect(dag[3][0].Name).To(Equal("mount-root"), actualDag)
+	Expect(dag[4][0].Name).To(Equal("mount-oem"), actualDag)
+	Expect(dag[5][0].Name).To(Equal("rootfs-hook"), actualDag)
+	Expect(dag[6][0].Name).To(Equal("load-config"), actualDag)
+	Expect(dag[7][0].Name).To(Or(Equal("overlay-mount"), Equal("custom-mount")), actualDag)
+	Expect(dag[7][1].Name).To(Or(Equal("overlay-mount"), Equal("custom-mount")), actualDag)
+	Expect(dag[8][0].Name).To(Equal("mount-bind"), actualDag)
+	Expect(dag[9][0].Name).To(Equal("write-fstab"), actualDag)
+}
