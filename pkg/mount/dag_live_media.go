4cbff2d8d4e98f263c62f81f3a19c655b99c4665
Itxaka <itxaka.garcia@spectrocloud.com>
2023-02-28 19:24:13 +0100

    Do not mount tmp on livecd/netboot (#65
    Signed-off-by: Itxaka <itxaka.garcia@spectrocloud.com>

--- pkg/mount/dag_live_media.go
+++ pkg/mount/dag_live_media.go
@@ -5,10 +5,9 @@ import (
 )
 
 // RegisterLiveMedia registers the dag for booting from live media/netboot
-// This mounts /tmp
+// This sets the sentinel
 func (s *State) RegisterLiveMedia(g *herd.Graph) error {
 	var err error
-	s.LogIfError(s.MountTmpfsDagStep(g), "tmpfs mount")
 	// Maybe LogIfErrorAndPanic ? If no sentinel, a lot of config files are not going to run
 	err = s.LogIfErrorAndReturn(s.WriteSentinelDagStep(g), "write sentinel")
 	return err
