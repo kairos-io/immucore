ec86832ca256b3cb26a05316772b4ac68b6691e3
Itxaka <itxaka.garcia@spectrocloud.com>
2023-03-03 15:44:06 +0100

    Always add the timeout to sysroot
    Signed-off-by: Itxaka <itxaka.garcia@spectrocloud.com>

--- dracut/28immucore/generator.sh
+++ dracut/28immucore/generator.sh
@@ -8,14 +8,9 @@ GENERATOR_DIR="$2"
 [ -z "$GENERATOR_DIR" ] && exit 1
 [ -d "$GENERATOR_DIR" ] || mkdir "$GENERATOR_DIR"
 
-cos_img=$(getarg cos-img/filename=)
-[ -z "${cos_img}" ] && exit 0
-
 # Add a timeout to the sysroot so it waits a bit for immucore to mount it properly
 mkdir -p "$GENERATOR_DIR"/sysroot.mount.d
 {
     echo "[Mount]"
     echo "TimeoutSec=300"
 } > "$GENERATOR_DIR"/sysroot.mount.d/timeout.conf
-
-## END GENERATE SYSROOT
