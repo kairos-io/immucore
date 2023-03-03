45faf3a908082b76d21f4541134759df40d01616
Itxaka <itxaka.garcia@spectrocloud.com>
2023-03-03 10:09:03 +0100

    WIP: dont depend on systemd
    Signed-off-by: Itxaka <itxaka.garcia@spectrocloud.com>

--- dracut/28immucore/module-setup.sh
+++ dracut/28immucore/module-setup.sh
@@ -2,14 +2,13 @@
 
 # called by dracut
 check() {
-    require_binaries "$systemdutildir"/systemd || return 1
-    return 255
+    # Always include this module
+    return 0
 }
 
 # called by dracut 
 depends() {
-    echo systemd rootfs-block dm fs-lib 
-    #tpm2-tss
+    echo rootfs-block dm fs-lib
     return 0
 }
 
@@ -29,15 +28,21 @@ install() {
     # add utils used by elemental or stages
     inst_multiple partprobe sync udevadm parted mkfs.ext2 mkfs.ext3 mkfs.ext4 mkfs.vfat mkfs.fat blkid e2fsck resize2fs mount umount sgdisk rsync
     # missing mkfs.xfs xfs_growfs in image?
-    inst_script "${moddir}/generator.sh" "${systemdutildir}/system-generators/immucore-generator"
-    inst_simple "${moddir}/immucore.service" "${systemdsystemunitdir}/immucore.service"
-    mkdir -p "${initdir}/${systemdsystemunitdir}/initrd-fs.target.requires"
-    ln_r "../immucore.service" "${systemdsystemunitdir}/initrd-fs.target.requires/immucore.service"
+    # systemd path
+    if dracut_module_included "systemd"; then
+      inst_script "${moddir}/generator.sh" "${systemdutildir}/system-generators/immucore-generator"
+      inst_simple "${moddir}/immucore.service" "${systemdsystemunitdir}/immucore.service"
+      mkdir -p "${initdir}/${systemdsystemunitdir}/initrd-fs.target.requires"
+      ln_r "../immucore.service" "${systemdsystemunitdir}/initrd-fs.target.requires/immucore.service"
 
-    # Until this is done on immucore, we need to ship it as part of the dracut module
-    inst_simple "${moddir}/kairos-setup-initramfs.service" "${systemdsystemunitdir}/kairos-setup-initramfs.service"
-    mkdir -p "${initdir}/${systemdsystemunitdir}/initrd.target.requires"
-    ln_r "../kairos-setup-initramfs.service" "${systemdsystemunitdir}/initrd.target.requires/kairos-setup-initramfs.service"
+      # Until this is done on immucore, we need to ship it as part of the dracut module
+      inst_simple "${moddir}/kairos-setup-initramfs.service" "${systemdsystemunitdir}/kairos-setup-initramfs.service"
+      mkdir -p "${initdir}/${systemdsystemunitdir}/initrd.target.requires"
+      ln_r "../kairos-setup-initramfs.service" "${systemdsystemunitdir}/initrd.target.requires/kairos-setup-initramfs.service"
+    else
+      inst_simple "${moddir}/immucore.rc" "/etc/init.d/immucore.rc"
+      # No idea how to force it to start?
+    fi
 
     dracut_need_initqueue
 }
