1e906fad66468115ac24200d46dac353f2f30818
Itxaka <itxaka@kairos.io>
2023-09-22 17:55:55 +0200

    Mount efi only if we are booting from disk
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- pkg/mount/dag_uki_boot.go
+++ pkg/mount/dag_uki_boot.go
@@ -2,6 +2,7 @@ package mount
 
 import (
 	cnst "github.com/kairos-io/immucore/internal/constants"
+	internalUtils "github.com/kairos-io/immucore/internal/utils"
 	"github.com/spectrocloud-labs/herd"
 )
 
@@ -22,7 +23,9 @@ func (s *State) RegisterUKI(g *herd.Graph) error {
 	s.LogIfError(s.UKIUdevDaemon(g), "udev")
 
 	// Mount ESP partition under efi if it exists
-	s.LogIfError(s.MountESPPartition(g, herd.WithDeps(cnst.OpSentinel, cnst.OpUkiUdev)), "mount ESP partition")
+	s.LogIfError(s.MountESPPartition(g, herd.EnableIf(func() bool {
+		return internalUtils.CheckEfiPartUUID() == nil
+	}), herd.WithDeps(cnst.OpSentinel, cnst.OpUkiUdev)), "mount ESP partition")
 
 	// Run rootfs stage
 	s.LogIfError(s.RootfsStageDagStep(g, herd.WithDeps(cnst.OpSentinel, cnst.OpUkiUdev)), "uki rootfs")
