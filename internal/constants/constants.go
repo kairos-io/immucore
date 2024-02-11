c9c9edb2613f6750f8f590b012dd83a9f6c682e1
Itxaka <itxaka.garcia@spectrocloud.com>
2024-02-11 18:37:24 +0100

    Mount cdrom efiboot contents under /run/rootfsbase (#224)
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -11,46 +11,48 @@ func GetCloudInitPaths() []string {
 	return []string{"/system/oem", "/oem/", "/usr/local/cloud-config/"}
 }
 
-// GenericKernelDrivers retusn a list of generic kernel drivers to insmod during uki mode
+// GenericKernelDrivers returns a list of generic kernel drivers to insmod during uki mode
 // as they could be useful for a lot of situations.
 func GenericKernelDrivers() []string {
-	return []string{"virtio", "ata_piix", "cdrom", "ext4", "iso9660", "usb_storage", "ahci",
-		"virtio_blk", "virtio_scsi", "virtio_net", "nvme", "overlay", "libata", "sr_mod", "simpledrm"}
+	return []string{"virtio", "ata_piix", "cdrom", "ext4", "iso9660", "usb_storage", "ahci", "fat", "vfat",
+		"virtio_blk", "virtio_scsi", "virtio_net", "nvme", "overlay", "libata", "sr_mod", "simpledrm", "loop"}
 }
 
 var ErrAlreadyMounted = errors.New("already mounted")
 
 const (
-	OpCustomMounts        = "custom-mount"
-	OpDiscoverState       = "discover-state"
-	OpMountState          = "mount-state"
-	OpMountBind           = "mount-bind"
-	OpMountRoot           = "mount-root"
-	OpOverlayMount        = "overlay-mount"
-	OpWriteFstab          = "write-fstab"
-	OpMountBaseOverlay    = "mount-base-overlay"
-	OpMountOEM            = "mount-oem"
-	OpRootfsHook          = "rootfs-hook"
-	OpInitramfsHook       = "initramfs-hook"
-	OpLoadConfig          = "load-config"
-	OpMountTmpfs          = "mount-tmpfs"
-	OpRemountRootRO       = "remount-ro"
-	OpUkiInit             = "uki-init"
-	OpSentinel            = "create-sentinel"
-	OpUkiUdev             = "uki-udev"
-	OpUkiBaseMounts       = "uki-base-mounts"
-	OpUkiKernelModules    = "uki-kernel-modules"
-	OpWaitForSysroot      = "wait-for-sysroot"
-	OpLvmActivate         = "lvm-activation"
-	OpKcryptUnlock        = "unlock-all"
-	OpKcryptUpgrade       = "upgrade-kcrypt"
-	OpUkiKcrypt           = "uki-unlock"
-	OpUkiMountLivecd      = "mount-livecd"
-	UkiLivecdMountPoint   = "/run/initramfs/live"
-	UkiLivecdPath         = "/dev/disk/by-label/UKI_ISO_INSTALL"
-	UkiDefaultcdrom       = "/dev/sr0"
-	UkiDefaultcdromFsType = "iso9660"
-	PersistentStateTarget = "/usr/local/.state"
-	LogDir                = "/run/immucore"
-	LinuxFs               = "ext4"
+	OpCustomMounts         = "custom-mount"
+	OpDiscoverState        = "discover-state"
+	OpMountState           = "mount-state"
+	OpMountBind            = "mount-bind"
+	OpMountRoot            = "mount-root"
+	OpOverlayMount         = "overlay-mount"
+	OpWriteFstab           = "write-fstab"
+	OpMountBaseOverlay     = "mount-base-overlay"
+	OpMountOEM             = "mount-oem"
+	OpRootfsHook           = "rootfs-hook"
+	OpInitramfsHook        = "initramfs-hook"
+	OpLoadConfig           = "load-config"
+	OpMountTmpfs           = "mount-tmpfs"
+	OpRemountRootRO        = "remount-ro"
+	OpUkiInit              = "uki-init"
+	OpSentinel             = "create-sentinel"
+	OpUkiUdev              = "uki-udev"
+	OpUkiBaseMounts        = "uki-base-mounts"
+	OpUkiKernelModules     = "uki-kernel-modules"
+	OpWaitForSysroot       = "wait-for-sysroot"
+	OpLvmActivate          = "lvm-activation"
+	OpKcryptUnlock         = "unlock-all"
+	OpKcryptUpgrade        = "upgrade-kcrypt"
+	OpUkiKcrypt            = "uki-unlock"
+	OpUkiMountLivecd       = "mount-livecd"
+	UkiLivecdMountPoint    = "/run/initramfs/live"
+	UkiIsoBaseTree         = "/run/rootfsbase"
+	UkiIsoBootImage        = "efiboot.img"
+	UkiLivecdPath          = "/dev/disk/by-label/UKI_ISO_INSTALL"
+	UkiDefaultcdrom        = "/dev/sr0"
+	UkiDefaultcdromFsType  = "iso9660"
+	UkiDefaultEfiimgFsType = "vfat"
+	PersistentStateTarget  = "/usr/local/.state"
+	LogDir                 = "/run/immucore"
 )
