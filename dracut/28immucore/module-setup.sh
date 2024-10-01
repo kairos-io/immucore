6cc4f86367b14403de61c727d80c48215d0abf00
Itxaka <itxaka@kairos.io>
2024-10-01 12:34:13 +0200

    Fail if dracut cant install required binaries
    Dracut inst_multiple does not fail if it cant find any of the required
    binaries, thus letting an initrafs in a broken state.
    
    This patch adds a function that will first check for those binaries and
    fail if it cant find them, then if it can find all of them calls the
    inst_multiple to install them
    
    Signed-off-by: Itxaka <itxaka@kairos.io>

--- dracut/28immucore/module-setup.sh
+++ dracut/28immucore/module-setup.sh
@@ -16,16 +16,27 @@ installkernel() {
     instmods overlay
 }
 
+# custom function to check if binaries exist before calling inst_multiple
+inst_check_multiple() {
+    for bin in "$@"; do
+        if ! command -v "$bin" >/dev/null 2>&1; then
+            derror "Required binary $bin not found!"
+            exit 1
+        fi
+    done
+    inst_multiple "$@"
+}
+
+
 # called by dracut
 install() {
     declare moddir=${moddir}
     declare systemdutildir=${systemdutildir}
     declare systemdsystemunitdir=${systemdsystemunitdir}
 
-    inst_multiple immucore
-    inst_multiple kairos-agent
+    inst_check_multiple immucore kairos-agent
     # add utils used by yip stages
-    inst_multiple partprobe sync udevadm parted mkfs.ext2 mkfs.ext3 mkfs.ext4 mkfs.vfat mkfs.fat blkid lsblk e2fsck resize2fs mount umount sgdisk rsync cryptsetup growpart sfdisk gawk awk
+    inst_check_multiple partprobe sync udevadm parted mkfs.ext2 mkfs.ext3 mkfs.ext4 mkfs.vfat mkfs.fat blkid lsblk e2fsck resize2fs mount umount sgdisk rsync cryptsetup growpart sfdisk gawk awk
 
     # Install libraries needed by gawk
     inst_libdir_file "libsigsegv.so*"
